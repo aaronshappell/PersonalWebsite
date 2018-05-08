51ecbfe725a862e6b2aae385cbcbd06dc2b79cfc
aaronshappell <crumster88@gmail.com>
2018-05-07 23:27:26 -0700

    Remove extra newline

--- src/main/kotlin/org/aaronshappell/PersonalWebsite/CVController.kt
+++ src/main/kotlin/org/aaronshappell/PersonalWebsite/CVController.kt
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
-
 class CVController {
     @GetMapping("/cv")
     fun cv(): String = "cv"
